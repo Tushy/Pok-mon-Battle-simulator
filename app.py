91646f47098a079b602da95d2fcabb4795a42cf4
Tushy <remco.dejong@student.hu.nl>
2016-11-07 13:05:35 +0100

    added moves
    Moves part added to base app

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import requests
 import json
 import csv
 import os
-import random
+from random import randint
 
 POKE_API = requests.get('http://pokeapi.co/api/v2')  # Vraagt de data aan.
 POKE_API_JSON = json.loads(POKE_API.text)  # Decode de JSON data.
@@ -31,6 +31,31 @@ def pokemon():
                         t.append(i['type']['name'])
                     for i in STATS_JSON['stats']:  # Voegt de verschillende stats met bijbehorende naam toe aan de dict met key=naam value=waarde van stat
                         stats.update({(i['stat']['name']): (i['base_stat'])})
+                    for i in pokemon_json['results']:
+                        MOVES = requests.get(i['url'])  # Vraagt een nieuwe URL aan.
+                        MOVES_JSON = json.loads(MOVES.text)  # Decode de data in de nieuwe URL.
+                        t = 0
+                        lst = []
+                        while t < 4:
+                            r = randint(0,len(MOVES_JSON['moves']))
+                            if r not in lst:
+                                lst.append(r)
+                                t += 1
+                            else:
+                                pass
+                        c = 0
+                        while c < 4:
+                            m = MOVES_JSON['moves'][lst[c]]
+                            MOVE = requests.get(m['move']['url'])  # Vraagt een nieuwe URL aan.
+                            MOVE_JSON = json.loads(MOVE.text)  # Decode de data in de nieuwe URL.
+
+
+                            print(m['move']['name'])
+                            print('type is: ' +MOVE_JSON['type']['name'])
+                            print('accuracy is: ' +str(MOVE_JSON['accuracy']))
+                            print('power is: ' +str(MOVE_JSON['power'] ))
+                            print('')
+                            c += 1
                     writer.writerow({'naam': naam, 'type': t, 'speed': stats['speed'], 'special-defense': stats['special-defense'], 'special-attack': stats['special-attack'], 'defense': stats['defense'], 'attack': stats['attack'], 'hp': stats['hp']}) # schrijft de waardes naar het bestand
     else:
         with open('./pokemon/%s.csv' % inputerino, encoding='utf-8', mode='r') as file:
