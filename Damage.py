727c3e260a66c2c6e246ee35a15901c257c41b3e
studenthu <a.tihouna@hotmail.com>
2016-11-09 11:47:17 +0100

    Update Damage.py

--- Damage.py
+++ Damage.py
@@ -1,32 +1,25 @@
 import csv
 
-with open('bulbasaurr.csv', 'w') as file:
-    fieldnames = ['Pokemon', 'Type', 'Speed', 'SpDef', 'SpAtt', 'Def', 'Att', 'Hp']
-    writer = csv.DictWriter(file, fieldnames=fieldnames, lineterminator='\n', delimiter=',')
-
-    writer.writeheader()
-    writer.writerow({'Pokemon': 'balbasaur', 'Type': 'poison, grass', 'Speed': '45', 'SpDef': '65', 'SpAtt': '65','Def': '49', 'Att': '49', 'Hp': '45'})
-
-with open('bulbasaurr.csv', 'r') as file:
-    r = csv.DictReader(file)
-    for stats in r:
-        Speed = int(stats['Speed'])
-        SpAtt = int(stats['SpAtt'])
-        Att = int(stats['Att'])
-        SpDef = int(stats['SpDef'])
-        Def = int(stats['Def'])
-        Hp = int(stats['Hp'])
-
+inputerino = input('naam: ')
 def damage():
-    effective_Hp = (SpDef + Def) * Hp
-    total_damage = (Speed + SpAtt + Att) * 1.4 * 10
+    with open('./pokemon/%s/stats.csv' % inputerino, 'r') as file:
+        r = csv.DictReader(file, delimiter=';')
+        for stats in r:
+            SpAtt = int(stats['special_attack'])
+            Att = int(stats['attack'])
+            SpDef = int(stats['special_defense'])
+            Def = int(stats['defense'])
+            Hp = int(stats['hp'])
 
-    return (effective_Hp - total_damage)
+    effective_Hp = (SpDef + Def) * Hp
+    total_damage = (SpAtt + Att) * 1.6 * 10
+
+    return effective_Hp - total_damage
+
 
 print(damage())
 
-
 '''
 Defence(HP) = (Sp DEF + DEF) * HP
-ATTACK = (Speed + Sp att + att * 1.6)  *  10
+ATTACK = (Sp att + att * 1.6)  *  10
 '''
