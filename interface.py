d44d00bac4df5f34c8dfd760c949da02ccf4251a
Falco91Bones <mjm.vanbrouwershaven@gmail.com>
2016-11-09 15:59:36 +0100

    Add files via upload

--- interface.py
+++ interface.py
@@ -1,6 +1,6 @@
 from tkinter import *
 from PIL import Image, ImageTk
-
+from all_pokemon_to_file import get_pokemons
 
 root = Tk()
 root.wm_title('PBS - Pokémon Battle Station')
@@ -10,11 +10,12 @@ root.state('zoomed')
 root.grid_columnconfigure(0, weight=1, uniform='HALF')
 root.grid_columnconfigure(1, weight=1, uniform='HALF')
 root.grid_rowconfigure(0, weight=1)
-root.geometry('1200x800')
+
 
 height = root.winfo_screenheight()
 width = root.winfo_screenwidth()
 
+#frames/ grid links en rechts
 left = Frame(root, height=height * 0.75, bd=5, relief=SUNKEN, background='red')
 right = Frame(root, height=height * 0.75, bd=5, relief=SUNKEN, background='blue')
 bottom = Frame(root, height=height * 0.25, bd=5, relief=SUNKEN, background='green')
@@ -23,10 +24,7 @@ right.grid(row=0, column=1, sticky='nesw', padx=40, pady=(40, 20))
 bottom.grid(row=1, columnspan=2, sticky='nesw', padx=40, pady=(20, 40))
 
 #background
-#image_background = Image.open('h.jpg')
-#image_background_load = ImageTk.PhotoImage(image_background)
 root.configure(background='yellow')
-
 
 # Label content
 pokemon_1_var = StringVar(root)
@@ -34,13 +32,17 @@ pokemon_2_var = StringVar(root)
 pokemon_1_var.set('Kies Pokémon 1')
 pokemon_2_var.set('Kies Pokémon 2')
 
-pokemon_1 = OptionMenu(left, pokemon_1_var, 'Pikachu', 'Bulbasaur', 'Charmender')
-pokemon_2 = OptionMenu(right, pokemon_2_var, 'Magikarp', 'Mew', 'Mewtwo')
+
+#drop down menu pokemon
+pokemon_1 = OptionMenu(left, pokemon_1_var, *get_pokemons()) #get_pokemons komt vanaf bestand all_pokemon_to_file.py
+pokemon_2 = OptionMenu(right, pokemon_2_var, *get_pokemons())
 pokemon_1.grid(row=0, column=0, sticky=NW)
 pokemon_2.grid(row=0, column=0, sticky=NW)
 
-image_1 = Image.open('pikachu.jpg')
-image_2 = Image.open('magikarp.jpg')
+
+#plaatjes pokeballs en pokemons
+image_1 = Image.open('pokeball.jpg')
+image_2 = Image.open('pokeball.jpg')
 image_1 = image_1.resize((200, 160), Image.ANTIALIAS)
 image_2 = image_2.resize((200, 160), Image.ANTIALIAS)
 pokemon_1_img_load = ImageTk.PhotoImage(image_1)
@@ -50,12 +52,13 @@ pokemon_2_img = Label(right, image=pokemon_2_img_load)
 pokemon_1_img.grid(row=1, column=0, sticky='nw')
 pokemon_2_img.grid(row=1, column=0, sticky='nw')
 
-pokemon_1_type = Label(left, text='Attacks Pokémon')
-pokemon_2_type = Label(right, text='Attacks Pokémon')
+#tekst onder plaatje
+pokemon_1_type = Label(left, text='MOVES POKÉMON')
+pokemon_2_type = Label(right, text='MOVES POKÉMON')
 pokemon_1_type.grid(row=2, column=0, sticky=NW)
 pokemon_2_type.grid(row=2, column=0, sticky=NW)
 
-# Stats kek
+# Stats
 pokemon_1_stats = ('HP', 'Attack Power')
 pokemon_2_stats = ('HP', 'Attack Power')
 pokemon_1_stats = Label(left, text='\n\n'.join(pokemon_1_stats))
@@ -63,15 +66,15 @@ pokemon_2_stats = Label(right, text='\n\n'.join(pokemon_2_stats))
 pokemon_1_stats.grid(row=1, column=1, sticky='nw')
 pokemon_2_stats.grid(row=1, column=1, sticky='nw')
 
-
+#buttons moves pokémon 1
 def p1_attack1():
-    listbox.insert('0', str('Pokemon 1 attack 1'))
+    listbox.insert('0', str(pokemon_1_var.get().upper() + ' is using attack 1'))
 def p1_attack2():
-    listbox.insert('0', str('Pokemon 1 attack 2'))
+    listbox.insert('0', str(pokemon_1_var.get().upper() + ' is using attack 2'))
 def p1_attack3():
-    listbox.insert('0', str('Pokemon 1 attack 3'))
+    listbox.insert('0', str(pokemon_1_var.get().upper() + ' is using attack 3'))
 def p1_attack4():
-    listbox.insert('0', str('Pokemon 1 attack 4'))
+    listbox.insert('0', str(pokemon_1_var.get().upper() + ' is using attack 4'))
 
 pokemon_1_move_1 = Button(left, padx=100, pady= 1, text='attack 1', command=p1_attack1)
 pokemon_1_move_1.grid(row=3, column=0,sticky=W )
@@ -85,15 +88,15 @@ pokemon_1_move_3.grid(row=4, column=0, sticky=W)
 pokemon_1_move_4 = Button(left, padx=100, pady= 1, text='attack 4', command=p1_attack4)
 pokemon_1_move_4.grid(row=4, column=1, sticky=W)
 
-
+#buttons moves pokémon 2
 def p2_attack1():
-    listbox.insert('0', str('Pokemon 2 attack 1'))
+    listbox.insert('0', str(pokemon_2_var.get().upper() + ' is using attack 1'))
 def p2_attack2():
-    listbox.insert('0', str('Pokemon 2 attack 2'))
+    listbox.insert('0', str(pokemon_2_var.get().upper() + ' is using attack 2'))
 def p2_attack3():
-    listbox.insert('0', str('Pokemon 2 attack 3'))
+    listbox.insert('0', str(pokemon_2_var.get().upper() + ' is using attack 3'))
 def p2_attack4():
-    listbox.insert('0', str('Pokemon 2 attack 4'))
+    listbox.insert('0', str(pokemon_2_var.get().upper() + ' is using attack 4'))
 
 pokemon_2_move_1 = Button(right, padx=100, pady= 1, text='attack 1', command=p2_attack1)
 pokemon_2_move_1.grid(row=3, column=0,sticky=W )
@@ -102,14 +105,16 @@ pokemon_2_move_2 = Button(right, padx=100, pady= 1, text='attack 2', command=p2_
 pokemon_2_move_2.grid(row=3, column=1,sticky=W)
 
 pokemon_2_move_3 = Button(right, padx=100, pady= 1, text='attack 3', command=p2_attack3)
-pokemon_2_move_3.grid(row=4, column=0, sticky=W)
+pokemon_2_move_3.grid(row=7, column=0, sticky=W)
 
 pokemon_2_move_4 = Button(right, padx=100, pady= 1, text='attack 4', command=p2_attack4)
-pokemon_2_move_4.grid(row=4, column=1, sticky=W)
+pokemon_2_move_4.grid(row=7, column=1, sticky=W)
 
 # Start functie
 def callback():
-    listbox.insert('0', str('Pokemon start your engines!'))
+    listbox.insert('0', str('Speler Red  kiest: ' + pokemon_1_var.get().upper() + '!'))
+    listbox.insert('0', str('Speler Blue kiest: ' + pokemon_2_var.get().upper() + '!'))
+
 
 #scroll text box
 scrollbar = Scrollbar(bottom)
