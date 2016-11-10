a9689f133a433a50d8d5cec044d523640ac80395
Tushy <Tushy@users.noreply.github.com>
2016-11-10 23:33:55 +0100

    Changed variable names and added comments
    Not all variable names have changed, but the majority should be readable test now. Also added some comments to explain the code. (in dutch!)

--- type_chart.py
+++ type_chart.py
@@ -8,31 +8,38 @@ POKE_TYPES_JSON = json.loads(POKE_TYPES.text)  # Decode de JSON data.
 
 
 def get_types():
-    lst = ['steel', 'shadow', 'unknown', 'fairy', 'dark']
-    type_folder = './pokemon/types/'
+    """Een functie voor het ophalen van de data voor de types pokemon, en de types aanvallen. Dit om te controleren of
+     een aanval super-effectief, niet effectief, of iets daar tussen in is"""
+    type_list = ['steel', 'shadow', 'unknown', 'fairy',
+                 'dark']  # Maakt een lijst aan met types die niet in de originele pokémon voor komen
+    type_folder = './pokemon/types/'  # Definieert de folder waar de data in moet worden opgeslagen
 
     for i in POKE_TYPES_JSON['results']:
         type_name = i['name']
-        if type_name not in lst:
+        if type_name not in type_list:
             if not os.path.exists(type_folder):
                 os.makedirs(type_folder)
 
             POKE_TYPE = requests.get(i['url'])  # Vraagt de data aan.
             POKE_TYPE_JSON = json.loads(POKE_TYPE.text)  # Decode de JSON data.
 
-            damage_types = {}
+            damage_types = {}  # Maakt een lege dict aan om de verschillende effectiviteiten van de aanvallen in op te slaan
 
             for t in POKE_TYPE_JSON['damage_relations']:
                 for i in POKE_TYPE_JSON['damage_relations'][t]:
                     if t == 'half_damage_from' or t == 'double_damage_from' or t == 'no_damage_from':
-                        if i['name'] not in lst:
+                        if i['name'] not in type_list:
                             if not t in damage_types:
                                 damage_types[t] = [(i['name'])]
                             else:
                                 damage_types[t].append(i['name'])
 
-            with open(type_folder + type_name + '.csv', 'w') as f:
+            with open(type_folder + type_name + '.csv',
+                      'w') as file:  # Schrijft de data weg naar de verschillende bestanden die corresponderen met de types
                 fieldnames = ['double_damage_from', 'half_damage_from', 'no_damage_from']
-                w = csv.DictWriter(f, fieldnames=fieldnames, delimiter=';')
-                w.writeheader()
-                w.writerow(damage_types)
+                writer = csv.DictWriter(file, fieldnames=fieldnames, delimiter=';')
+                writer.writeheader()
+                writer.writerow(damage_types)
+
+
+get_types()
