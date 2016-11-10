88e79731b58dcf250ead89c0b3ef26621c571910
Tushy <Tushy@users.noreply.github.com>
2016-11-10 12:21:25 +0100

    Commented and removed variable

--- ping.py
+++ ping.py
@@ -2,7 +2,11 @@ import requests
 
 
 def ping():
+    '''
+    Ping naar de server van de API om te controlleren of er een internet verbinding beschikbaar is. Als dit niet zo is
+    returned er een string voor in de text box van het interface'
+    '''
     try:
-        ping = requests.get('http://pokeapi.co/api/v2/')
+        requests.get('http://pokeapi.co/api/v2/')
     except:
         return 'U heeft internet nodig voor dit programma'
