9081f9192a317b3ff102ae0dbf32571ad2432f01
SizmorlimMudbreaker <nickswaerdens@gmail.com>
2016-11-11 11:33:51 +0100

    Add files via upload

--- type_chart.py
+++ type_chart.py
@@ -14,25 +14,26 @@ def get_types():
                  'dark']  # Maakt een lijst aan met types die niet in de originele pokémon voor komen
     type_folder = './pokemon/types/'  # Definieert de folder waar de data in moet worden opgeslagen
 
-    for i in POKE_TYPES_JSON['results']:
-        type_name = i['name']
+    for item in POKE_TYPES_JSON['results']:
+        type_name = item['name']
         if type_name not in type_list:
             if not os.path.exists(type_folder):
                 os.makedirs(type_folder)
 
-            POKE_TYPE = requests.get(i['url'])  # Vraagt de data aan.
+            POKE_TYPE = requests.get(item['url'])  # Vraagt de data aan.
             POKE_TYPE_JSON = json.loads(POKE_TYPE.text)  # Decode de JSON data.
 
             damage_types = {}  # Maakt een lege dict aan om de verschillende effectiviteiten van de aanvallen in op te slaan
 
-            for t in POKE_TYPE_JSON['damage_relations']:
-                for i in POKE_TYPE_JSON['damage_relations'][t]:
-                    if t == 'half_damage_from' or t == 'double_damage_from' or t == 'no_damage_from':
-                        if i['name'] not in type_list:
-                            if not t in damage_types:
-                                damage_types[t] = [(i['name'])]
+            # Zoekt de damage types en hoeveel damage deze doen op een bepaalde type pokemon.
+            for type in POKE_TYPE_JSON['damage_relations']:
+                for damage_name in POKE_TYPE_JSON['damage_relations'][type]:
+                    if type == 'half_damage_from' or type == 'double_damage_from' or type == 'no_damage_from':
+                        if damage_name['name'] not in type_list:
+                            if not type in damage_types:
+                                damage_types[type] = [(damage_name['name'])]
                             else:
-                                damage_types[t].append(i['name'])
+                                damage_types[type].append(damage_name['name'])
 
             with open(type_folder + type_name + '.csv',
                       'w') as file:  # Schrijft de data weg naar de verschillende bestanden die corresponderen met de types
@@ -40,6 +41,3 @@ def get_types():
                 writer = csv.DictWriter(file, fieldnames=fieldnames, delimiter=';')
                 writer.writeheader()
                 writer.writerow(damage_types)
-
-
-get_types()
